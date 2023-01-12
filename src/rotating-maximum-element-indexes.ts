928e4c59a44507c442529262e8ecb4e8d72b63d5
Agung Insani <agunginsanialam@gmail.com>
2023-01-12 19:38:31 +0700

    refactor: simply code

--- src/rotating-maximum-element-indexes.ts
+++ src/rotating-maximum-element-indexes.ts
@@ -21,18 +21,23 @@ export function normal(as: Array<number>, rotate: Array<number>) {
 export function recursive(
   as: Array<number>,
   rotate: Array<number>,
-  results: Array<number> = [],
-  maxIndex?: number
+  results: Array<number> = []
 ) {
-  const getMaxIndex = (i = 0, max = 0) => {
+  function getMaxIndex(i = 0, max = 0) {
     if (i === as.length) return max;
     return getMaxIndex(i + 1, as[i] > as[max] ? i : max);
-  };
-  if (rotate.length === 0) return results;
-  const [h, ...t] = rotate;
-  const rotateModifier = h % as.length;
-  const _maxIndex = getMaxIndex();
-  const temp = (maxIndex ?? _maxIndex) - rotateModifier;
-  const index = temp < 0 ? temp + as.length : temp;
-  return recursive(as, t, [...results, index], _maxIndex);
+  }
+
+  const maxIndex = getMaxIndex();
+
+  function getResults(rotate: Array<number>) {
+    if (rotate.length === 0) return results;
+    const [h, ...t] = rotate;
+    const rotateModifier = h % as.length;
+    const temp = maxIndex - rotateModifier;
+    const index = temp < 0 ? temp + as.length : temp;
+    return recursive(as, t, [...results, index]);
+  }
+
+  return getResults(rotate);
 }
