b6ca4dd40fba021551bb3f24fbcfba62aa36a451
Agung Insani <agunginsanialam@gmail.com>
2023-01-12 21:39:19 +0700

    fix: fix typo

--- src/rotating-maximum-element-indexes.ts
+++ src/rotating-maximum-element-indexes.ts
@@ -18,11 +18,7 @@ export function normal(as: Array<number>, rotate: Array<number>) {
   return results;
 }
 
-export function recursive(
-  as: Array<number>,
-  rotate: Array<number>,
-  results: Array<number> = []
-) {
+export function recursive(as: Array<number>, rotate: Array<number>) {
   function getMaxIndex(i = 0, max = 0) {
     if (i === as.length) return max;
     return getMaxIndex(i + 1, as[i] > as[max] ? i : max);
@@ -30,13 +26,13 @@ export function recursive(
 
   const maxIndex = getMaxIndex();
 
-  function getResults(rotate: Array<number>) {
+  function getResults(rotate: Array<number>, results: Array<number> = []) {
     if (rotate.length === 0) return results;
     const [h, ...t] = rotate;
     const rotateModifier = h % as.length;
     const temp = maxIndex - rotateModifier;
     const index = temp < 0 ? temp + as.length : temp;
-    return recursive(as, t, [...results, index]);
+    return getResults(t, [...results, index]);
   }
 
   return getResults(rotate);
